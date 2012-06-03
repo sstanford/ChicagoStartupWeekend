e0d0134fbcf4c6906e857eddb5c6301afcf6fb2e
Shane Stanford <shane@groupon.com>
2012-06-03 15:44:17 -0500

    asda

--- lib/load_data.rb
+++ lib/load_data.rb
@@ -4,27 +4,27 @@ module LoadData
       pl.destroy
     end
     PayLevel.create!( amount: 1,
-                      photo_url: 'http://www.belated.co/granny.gif',
-                      description: 'Test description',
+                      photo_url: 'http://www.belated.co/images/graphic_opt1.png',
+                      description: '',
                       message: "I'm a bit of a jerk",
                       name: 'Test name')
     PayLevel.create!( amount: 2,
-                      photo_url: '',
+                      photo_url: 'http://www.belated.co/images/graphic_opt2.png',
                       description: '',
                       message: "'I'm sorta of a jerk",
                       name: '')
     PayLevel.create!( amount: 5,
-                      photo_url: '',
+                      photo_url: 'http://www.belated.co/images/graphic_opt3.png',
                       description: '',
                       message: "'I'm a jerk",
                       name: '')
     PayLevel.create!( amount: 10,
                       photo_url: '',
-                      description: '',
+                      description: 'http://www.belated.co/images/graphic_opt4.png',
                       message: "'I'm really a jerk",
                       name: '')
     PayLevel.create!( amount: 20,
-                      photo_url: '',
+                      photo_url: 'http://www.belated.co/images/graphic_opt5.png',
                       description: '',
                       message: "'I'm an asshole",
                       name: '')
