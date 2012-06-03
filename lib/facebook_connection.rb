09a91bf84d540f2d67864eee8e878c65cb124216
Shane Stanford <shane@groupon.com>
2012-06-02 23:22:03 -0500

    facebook

--- lib/facebook_connection.rb
+++ lib/facebook_connection.rb
@@ -3,5 +3,6 @@ module FacebookConnection
     @fb_auth = FbGraph::Auth.new(FACEBOOK_SETTINGS[:app_id], FACEBOOK_SETTINGS[:app_secret])
     client = @fb_auth.client
     client.redirect_uri = "http://localhost:3000/end_login"
+    client
   end
 end
